0c7c5edb835a6e5324f2a81ada8c4ca0c3acc7aa
ChernihTan <lvovn62@gmail.com>
2024-01-29 13:04:31 +0200

    HW Wolf-ver2, WolfTest-ver2
    Создала геттеры и сеттеры

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,72 @@
+public class Wolf {
+    private char sex;
+    private String nickName;
+    // вес в кг
+    private float weight;
+    private int age;
+    private String colour;
+
+    // Методы геттеры и сеттеры
+    // пол животного
+    public char getSex() {
+        return sex;
+    }
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    // кличка
+    public String getNickName() {
+        return nickName;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    // окрас
+    public String getColour() {
+        return colour;
+    }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+    // вес в кг
+    public float getWeight() {
+        return weight;
+    }
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    // возраст животного
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        //возраст волка не может быть > 8 лет
+        if(age > 8) {
+            System.out.println("Возраст волка не может быть больше 8 лет!");
+        } else {
+            this.age = age;
+        }
+    }
+
+    // Методы, описывающие поведение (что он может делать) волка:
+    // идти, сидеть, бежать, выть, охотиться
+    public void go() {
+        System.out.println("Идет!");
+    }
+
+    public void sit() {
+        System.out.println("Сидит!");
+    }
+
+    public void run() {
+        System.out.println("Бежит!");
+    }
+
+    public void howl() {
+        System.out.println("Воет!");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится!");
+    }
+}
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,84 +1,37 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf akela = new Wolf();
-        akela.sex = 'К';
-        akela.nickName = "Акела";
-        akela.weight = 49.5f;
-        akela.age = 15;
-        akela.colour = "серый";
-        System.out.println("Мы создали волка по кличке " + akela.nickName + ", возраст - " +
-                 akela.age + (akela.age >= 5 ? " лет" : (akela.age == 1 ? " год" : " года")) +
-                 "\nЭто " + (akela.sex == 'К' ? "волк" : "волчица") + " весом " +
-                 akela.weight + " кг. Окрас - " + akela.colour + ".");
-        System.out.println(akela.nickName + " сейчас:");
+        akela.setSex('К');
+        akela.setNickName("Акела");
+        akela.setWeight(49.5f);
+        akela.setAge(15);
+        akela.setColour("серый");
+        System.out.println("Мы создали волка по кличке " + akela.getNickName() + ", возраст - " +
+                 akela.getAge() + (akela.getAge() >= 5 ? " лет" : (akela.getAge() == 1 ? " год" : " года")) +
+                 "\nЭто " + (akela.getSex() == 'К' ? "волк" : "волчица") + " весом " +
+                 akela.getWeight() + " кг. Окрас - " + akela.getColour() + ".");
+        System.out.println(akela.getNickName() + " сейчас:");
         akela.hunt();
         akela.run();
-        System.out.println("Вечером " + akela.nickName + ":");
+        System.out.println("Вечером " + akela.getNickName() + ":");
         akela.sit();
         akela.howl();
 
         Wolf raksha = new Wolf();
-        raksha.sex = 'С';
-        raksha.nickName = "Ракша";
-        raksha.weight = 42.5f;
-        raksha.age = 3;
-        raksha.colour = "серый";
-        System.out.println("\nМы создали волка по кличке " + raksha.nickName + ", возраст - " +
-                 raksha.age + (raksha.age >= 5 ? " лет" : (raksha.age == 1 ? " год" : " года")) +
-                 "\nЭто " + (raksha.sex == 'К' ? "волк" : "волчица") + " весом " +
-                 raksha.weight + " кг. Окрас - " + raksha.colour + ".");
-        System.out.println(raksha.nickName + " сейчас:");
+        raksha.setSex('С');
+        raksha.setNickName("Ракша");
+        raksha.setWeight(42.5f);
+        raksha.setAge(3);
+        raksha.setColour("серый");
+        System.out.println("\nМы создали волка по кличке " + raksha.getNickName() + ", возраст - " +
+                 raksha.getAge() + (raksha.getAge() >= 5 ? " лет" : (raksha.getAge() == 1 ? " год" : " года")) +
+                 "\nЭто " + (raksha.getSex() == 'К' ? "волк" : "волчица") + " весом " +
+                 raksha.getWeight() + " кг. Окрас - " + raksha.getColour() + ".");
+        System.out.println(raksha.getNickName() + " сейчас:");
         raksha.go();
         raksha.hunt();
-        System.out.println("Вечером " + raksha.nickName + ":");
+        System.out.println("Вечером " + raksha.getNickName() + ":");
         raksha.sit();
         raksha.howl();
     }
 }
-
-public class Wolf {
-    // Поля (атрибуты), описывающие характеристики волка
-    //пол, кличка, вес, возраст, окрас
-    char sex;
-    String nickName;
-    // вес в кг
-    float weight;
-    int age;
-    String colour;
-
-    // Методы, описывающие поведение (что он может делать) волка:
-    // идти, сидеть, бежать, выть, охотиться
-    public void go() {
-        System.out.println("Идет!");
-    }
-
-    public void sit() {
-        System.out.println("Сидит!");
-    }
-
-    public void run() {
-        System.out.println("Бежит!");
-    }
-
-    public void howl() {
-        System.out.println("Воет!");
-    }
-
-    public void hunt() {
-        System.out.println("Охотится!");
-    }
-
-    
-}
-/*Создайте класс Wolf
-объявите в нем поля: пол, кличка, вес, возраст, окрас (не присваивайте им значения по умолчанию)
-реализуйте методы: идти, сидеть, бежать, выть, охотиться
-в каждом методе выводите сообщение вида: "Воет", "Сидит" и т. д.
-Создайте класс WolfTest с методом main
-в нем создайте объект типа Wolf
-в этом же классе присвойте полям Wolf какие-то значения
-получите эти значения из полей и отобразите в консоли
-вызовите методы объекта
-*/
-
-
