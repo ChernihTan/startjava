d3bc244e9102f2d209be86be682f311bb0baa60c
ChernihTan <lvovn62@gmail.com>
2024-05-25 00:18:07 +0300

    HW Исправление Calculator.java и CalculatorTest.java
    Исправления по замечаниям

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,55 +1,111 @@
 package com.startjava.lesson_2_3_4.calculator;
+
 public class Calculator {
+    static final int CONSTANTA_ELEMENTS_IN_EXPRESSION = 3;
+    private static int a;
+    private static int b;
+    private static char sign;
     // метод отвечает за проверку математического знака и сами вычисления
-    public static void calculate(String expression) throws NumberFormatException, ArithmeticException,
-            SignException, UnspacedExpression, NegativeNumber, ArrayIndexOutOfBoundsException
-    {
-        int a = 0;
-        int b = 0;
-        char sign = ' ';
+
+    public static double calculate(String expression) throws NumberFormatException, ArithmeticException,
+            SignException, UnspacedExpression, NegativeNumber {
+        a = 0;
+        b = 0;
+        sign = ' ';
         // Разбор выражения
-        String[] words = expression.split(" ");  //(" |\\\n");
-        String[] arr = new String[3];
-        double result = Double.NaN;
-        // Убираем пустые строки, если случайно набрали несколько пробелов подряд
-        if (words.length < 3) {
+        String[] args = expression.split(" ");  // (" |\\\n");
+        int length = args.length;
+
+        // если случайно набрали несколько пробелов подряд,
+        // не пустые элементы элементы массива переношу в начало массива
+        if (length < CONSTANTA_ELEMENTS_IN_EXPRESSION) {
             // генерирую исключение
             throw new UnspacedExpression("Ошибка: выражение введено без пробелов!\n");
         }
-        int j = 0;
-        for (int i = 0; i < words.length; i++) {
-            if (!words[i].isEmpty()) {
-                arr[j++] = words[i];
-            }
-        }
-        a = java.lang.Integer.parseInt(arr[0]);
-        b = java.lang.Integer.parseInt(arr[2]);
-        if(a < 0 || b < 0) {
-            throw new NegativeNumber("Введено отрицательное число!\n");
-        }
-        sign = arr[1].charAt(0);
-
-        // Выполнение операции (+, -, *, /, ^, %)
-        result = switch (sign) {
-            case '+' -> a + b;
-            case '-' -> a - b;
-            case '*' -> a * b;
-            case '/' -> {
-                if ((double) a / b == Double.POSITIVE_INFINITY) { //Infinity
-                    throw new ArithmeticException("Ошибка: делить на ноль нельзя!\n");
+        // вариант 1 избавления от лишних пробелов
+        for (int i = 0; i < length - 1; i++) {
+            int j;
+            // если текущий элемент пустой, ищу далее первый не пустой
+            try {
+                if (args[i].isEmpty()) {
+                    j = i;
+                    do {
+                        j++;
+                    } while (args[j].isEmpty());
+                    args[i] = args[j];
+                    args[j] = "";
                 }
-                yield (double) a / b;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
             }
+        }
+
+        // Вариант второй избавления от личших пробелов
+        String[] argsNew = expression.split(" ");  // (" |\\\n");
+        for (int i = 0; i < length - 1; i++) {
+            int j;
+            // если текущий элемент пустой, ищу далее первый не пустой
+            if (argsNew[i].isEmpty()) {
+                j = i;
+                do {
+                    if (++j == length) {
+                        break;
+                    }
+                } while (argsNew[j].isEmpty());
+                if (j == length) {
+                    break;
+                }
+                argsNew[i] = argsNew[j];
+                argsNew[j] = "";
+            }
+        }
+
+        a = defineArgument(args[0]);
+        b = defineArgument(args[2]);
+        sign = args[1].charAt(0);
+
+        // проверка на 0 второго аргумента, если операция деления
+        if (b == 0 && (sign == '/' || sign == '%')) {
+            throw new ArithmeticException("Ошибка: делить на ноль нельзя!\n");
+        }
+        // Выполнение операции (+, -, *, /, ^, %)
+
+        double result = switch (sign) {
+            case '+' -> (double) a + b;
+            case '-' -> (double) a - b;
+            case '*' -> (double) a * b;
+            case '/' -> (double) a / b;
             case '^' -> Math.pow(a, b);
-            case '%' -> a % b;
+            case '%' -> (double) a % b;
             default -> throw new SignException("Ошибка: знак " + sign + " не поддерживается!\n");
         };
-        printResult(a, sign, b, result);
+        return result;
     }
+
     // печать результата
     public static void printResult(int a, char sign, int b, double result) {
         System.out.print(a + " " + sign + " " + b + " = ");
         System.out.printf((result % 1 > 0) ? ("%.3f%n") : ("%.0f%n"), result);
         System.out.println();
+    }
+
+    private static int defineArgument(String argument) throws NegativeNumber {
+        int number = java.lang.Integer.parseInt(argument);
+        if (number < 0) {
+            throw new NegativeNumber("Введено отрицательное число!\n");
+        }
+        return number;
+    }
+
+    public static int getA() {
+        return a;
+    }
+
+    public static int getB() {
+        return b;
+    }
+
+    public static char getSign() {
+        return sign;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,34 +3,38 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
+
     public static void main(String[] args) throws SignException {
         Scanner scanner = new Scanner(System.in);
         String answer;
         String mathExpression;
+        double result;
         do {
             System.out.print("\nВведите математическое выражение: ");
             mathExpression = scanner.nextLine();
             try {
-                Calculator.calculate(mathExpression);
+                result = Calculator.calculate(mathExpression);
+                Calculator.printResult(Calculator.getA(), Calculator.getSign(), Calculator.getB(), result);
             } catch (NumberFormatException e) {
-                // e.printStackTrace();
-                String message = e.getMessage();
-                int index1 = message.indexOf('"') + 1; // 2
-                int index2 = message.lastIndexOf('"'); // 19 "test"
-                String substring = message.substring(index1, index2); // 23
-                // message.split(" ")
-                System.out.println("Ошибка: неверный формат целого числа - " + substring + "\n");
+                System.out.println("Ошибка: неверный формат целого числа\n");
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Длинное выражение, должно быть два числа и между ними " +
                         "знак математической операции, разделенные пробелом.\n");
             } catch (NegativeNumber | ArithmeticException | SignException | UnspacedExpression e) {
                 System.out.println(e.getMessage());
             }
+
             // жду только правильного ввода
-            do {
-                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                answer = scanner.nextLine().toUpperCase();
-            } while (!answer.equals("NO") && !answer.equals("YES"));
+            //System.out.print("Если не хотите продолжить вычисления, введите no: ");
+            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+            answer = scanner.nextLine().toUpperCase();
+            if (!answer.equals("YES")) {
+                answer = "NO";
+            }
+//            do {
+//                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+//                answer = scanner.nextLine().toUpperCase();
+//            } while (!answer.equals("NO") && !answer.equals("YES"));
         } while (!answer.equals("NO"));
         System.out.println("Программа завершена");
     }
